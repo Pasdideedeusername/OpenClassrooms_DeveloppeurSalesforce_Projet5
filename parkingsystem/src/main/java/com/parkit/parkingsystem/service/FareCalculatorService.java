2b1d9fa545eefffcd0970cf99fb8c77d6e4565c3
Carine Flood <dk3s4t46r762-student@anonymous-codio.com>
2023-02-28 16:43:39 +0000

    implementation de la fonctionnalité des 5% de remise - étape 4 Codio

--- parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ parkingsystem/src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -1,11 +1,13 @@
-package com.parkit.parkingsystem.service;
+cdpackage com.parkit.parkingsystem.service;
 
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
 public class FareCalculatorService {
+    
+    public void calculateFare(Ticket ticket, boolean discount){
 
-    public void calculateFare(Ticket ticket){
+//    public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
             throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
@@ -28,17 +30,31 @@ public class FareCalculatorService {
         if (duration <= 0.5) { 
             duration = 0;
         }
-        
+   
+    
+    // implémentation du discount pour l'étape 4:
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
                 ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                
                 break;
             }
             case BIKE: {
                 ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+ 
                 break;
             }
-            default: throw new IllegalArgumentException("Unkown Parking Type");
+            
+           default: throw new IllegalArgumentException("Unkown Parking Type");
         }
+        if (discount) {
+            ticket.setPrice(ticket.getPrice() * 0.95);
+        }
+    }  
+      // rajouté par Carine à l'étape 4. ICI JE GENERE UNE ERREUR
+    public void calculateFare(Ticket ticket){
+        this.calculateFare(ticket, false);
     }
+
 }
+
