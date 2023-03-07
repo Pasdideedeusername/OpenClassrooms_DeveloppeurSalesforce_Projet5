8e0e6fc0fc2c67677a2e54f1e6d6d5d1d7e209ff
Carine Flood <dk3s4t46r762-student@anonymous-codio.com>
2023-03-07 08:17:37 +0000

    modification du test processExitingVehicleTest - étape 5

--- parkingsystem/src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ parkingsystem/src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -21,30 +21,28 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 public class ParkingServiceTest {
 
-    private static ParkingService parkingService;
+    private static ParkingService parkingService; //Carine(pour comprendre): c'est la classe testée
 
     @Mock
-    private static InputReaderUtil inputReaderUtil;
+    private static InputReaderUtil inputReaderUtil; //Carine: classe mockée: sert à lire extérieur
     @Mock
-    private static ParkingSpotDAO parkingSpotDAO;
+    private static ParkingSpotDAO parkingSpotDAO; //Carine(pour comprendre): classe mockée qui sert à chercher le N°parking et modif libre/occupé
     @Mock
-    private static TicketDAO ticketDAO;
+    private static TicketDAO ticketDAO; //Carine(pour comprendre): classe mockée qui sert à éditer le ticket (get/save/update)
 
     @BeforeEach
     private void setUpPerTest() {
         try {
             when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
 
-            ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false);
-            Ticket ticket = new Ticket();
+            ParkingSpot parkingSpot = new ParkingSpot(1, ParkingType.CAR,false); //Carine(pour comprendre): la classe ParkingSpot dans model porte uniquement des données en entrée et en sortie, on ne mocke pas !
+            Ticket ticket = new Ticket(); //Carine(pour comprendre): classe Ticket comme ParkingSpot dans model
             ticket.setInTime(new Date(System.currentTimeMillis() - (60*60*1000)));
             ticket.setParkingSpot(parkingSpot);
             ticket.setVehicleRegNumber("ABCDEF");
             when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
-            when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
-
-            when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);
-
+            when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);//Carine (pour comprendre): indique qu'il y a bien un tiquet mis à jour?
+            when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(true);//Carine (pour comprendre): indique que parkingSpot is available ?
             parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         } catch (Exception e) {
             e.printStackTrace();
@@ -52,10 +50,13 @@ public class ParkingServiceTest {
         }
     }
 
-    @Test
+    @Test // test à compléter de l'étape 5
     public void processExitingVehicleTest(){
+        when(ticketDAO.getNbTicket(anyString())).thenReturn(3);// créé par Carine pour mocker getNbticket()
         parkingService.processExitingVehicle();
         verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
-    }
+        verify(ticketDAO, Mockito.times(1)).getNbTicket(anyString());
+     }
 
+    
 }
